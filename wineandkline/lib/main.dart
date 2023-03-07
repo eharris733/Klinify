964f189234524aa70bc2810a86e3f3c85add5186
eharris733 <elliotmharris@gmail.com>
2023-03-06 20:09:54 -0500

    basic change

--- wineandkline/lib/main.dart
+++ wineandkline/lib/main.dart
@@ -77,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.green,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Elliot's Change"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
